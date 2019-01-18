f3d5c3d382cfbf3bf138d1b79ae724057c99f0d1
Valen Yamamoto <valenryamamoto@gmail.com>
2019-01-18 15:05:49 -0800

    Vector class

--- src/pathCreation/Vector.java
+++ src/pathCreation/Vector.java
@@ -1,5 +1,50 @@
 package pathCreation;
 
 public class Vector {
-
+	
+	private double x;
+	private double y;
+	
+	/**
+	 * Contructor for Vector with two points
+	 * @param a starting point
+	 * @param b second point
+	 */
+	public Vector(Point a, Point b) {
+		this.x = b.getX() - a.getX();
+		this.y = b.getY() - a.getY();
+	}
+	
+	public Vector(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double magnitude() {
+		return Math.sqrt(x*x + y*y);
+	}
+	
+	public Vector normalize() {
+		double mag = magnitude();
+		Vector normal = new Vector(x, y);
+		normal.setX(x/mag);
+		normal.setY(y/mag);
+		return normal;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	public void setY(double y) {
+		this.y = y;
+	}
 }
